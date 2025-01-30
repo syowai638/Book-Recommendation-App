76511cfa49c0825822bc7159c3ac2b3b3fe80108
Michell Wambui Mbogo <mishie254@gmail.com>
2025-01-30 22:20:45 +0300

    g6

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer () {
     return (
         <div>
             <footer id="footer">
-        <p >&copy; 2025  All rights reserved</p>
+        <p >&copy; 2025  Group 6  All rights reserved</p>
         </footer>
         </div>
     )
