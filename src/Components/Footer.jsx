966a4498f53eb12ca4d98e82c4de18c005d99756
Michell Wambui Mbogo <mishie254@gmail.com>
2025-01-30 17:04:23 +0300

    footer jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <div>
+            <footer id="footer">
+        <p >&copy; 2025  All rights reserved</p>
+        </footer>
+        </div>
+    )
+}export default Footer;
