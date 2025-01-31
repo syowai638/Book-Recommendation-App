8f22677e30fe6af3eecb845dbd058a75f3aebe37
ArnoldMajor <87968008+ArnoldMajor@users.noreply.github.com>
2025-01-31 14:31:03 +0300

    Update BookList.jsx

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -8,10 +8,6 @@ function BookList({ books }) {
                 {books.map(book => (
                     <div key={book.id} className='book-card'>
                         <Link to={`/books/${book.id}`} className='book-link'>
-                           <div className="book-tags">
-                           <p className="tag rated">Highly Rated</p>
-                           <p className="tag recommended">Recommended</p> 
-                           </div>
                             <img src={book.imageURL} alt={book.title} className='book-thumbnail' />
                             <h2>{book.title}</h2>
                         </Link>
