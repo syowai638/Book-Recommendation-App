3d550e09029ae47e2b71ff1f2d88ec458c7a59fa
prudence <prudencec2019@gmail.com>
2025-02-01 09:09:03 +0300

    Update BookList.jsx

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,27 +1,33 @@
 import { Link } from 'react-router-dom';
 
+
 function BookList({ books }) {
-    return (
-        <main>
-            <h1>Book List</h1>
-            <div className='book-list'>
-                {books.map(book => (
-                    <div key={book.id} className='book-card'>
-                        <Link to={`/books/${book.id}`} className='book-link'>
+   return (
+       <main>
+           <h1>Book List</h1>
+           <div className='book-list'>
+               {books.map(book => (
+                   <div key={book.id} className='book-card'>
+                       <Link to={`/books/${book.id}`} className='book-link'>
                            <div className="book-tags">
-                           <p className="tag rated">Highly Rated</p>
-                           <p className="tag recommended">Recommended</p> 
                            </div>
-                            <img src={book.imageURL} alt={book.title} className='book-thumbnail' />
-                            <h2>{book.title}</h2>
-                        </Link>
-                    </div>
-                ))
-                }
-            </div >
-        </main >
-    );
+                           <img src={book.imageURL} alt={book.title} className='book-thumbnail' />
+                           <div className='info-section'>
+                               <h2 className='book-title'>{book.title}</h2>
+                               <p>{book.rating}</p>
+                           </div>
+                       </Link>
+                   </div>
+               ))
+               }
+           </div >
+       </main >
+   );
 }
 
 
 export default BookList;
+
+
+
+
