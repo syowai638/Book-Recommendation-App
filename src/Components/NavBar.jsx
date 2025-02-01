e5827789f84188886b44fa58b69eb0a2fd13a59b
michellmbogo <113033732+michellmbogo@users.noreply.github.com>
2025-02-01 09:11:14 +0300

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-function Navbar (){
-      
+import BookSearch from "./BookSearch";
 
-    return (
-        <div>
-            <nav id='navbar'>
-                <h1 id='title'>Book Recommendation App</h1>
-                <div className="navbar-menu">
-                    <NavLink to='/home' className='nav-link'>Home</NavLink>
-                    <NavLink to='/ratingform' className='nav-link'>Rating</NavLink>
-                </div>
-                <input type="text" placeholder="Search"/>
-            </nav>
-            
-        </div>
-    )
-}export default Navbar;
+
+function Navbar({ searchTerm, onSearch }) {
+
+
+   return (
+       <div>
+           <nav id='navbar'>
+               <h1 id='title'>Book Recommendation App</h1>
+               <div className="navbar-menu">
+                   <NavLink to='/home' className='nav-link'>Home</NavLink>
+                   <NavLink to='/ratingform' className='nav-link'>Rating</NavLink>
+               </div>
+               <BookSearch searchTerm={searchTerm} onSearch={onSearch} />
+           </nav>
+
+
+       </div>
+   )
+}
+
+
+export default Navbar;
